8484591dea695a762a54be5012a87a4f611d8c58
Tabassum613 <75952326+Tabassum613@users.noreply.github.com>
2024-09-18 22:55:36 +0600

    2nd Answer of the Question

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ const result=calculateDifference(10,4);
 console.log(result);
 
 
+//2.Write a function named isOdd that takes one argument and returns true if the number is odd, and false if it is not.
+
+
+function isOdd(num) {
+    return num % 2 !== 0;
+}
+
+console.log(isOdd(9));
+console.log(isOdd(10));
+
+
