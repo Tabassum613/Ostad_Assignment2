eb9e4cc582543aa15b304d253d7a354ab56facd7
Tabassum613 <75952326+Tabassum613@users.noreply.github.com>
2024-09-18 22:52:45 +0600

    1st Answer of the Question

---
+++ index.js
@@ -0,0 +1,9 @@
+//1) Write a function named calculateDifference that takes two arguments and returns the difference between the first and second argument.
+
+function calculateDifference(a, b) {
+    return a - b;
+}
+const result=calculateDifference(10,4);
+console.log(result);
+
+
